0ddf9e8a313e051264cb972a31f54bf849fa132b
tundak <sonu5650@gmail.com>
2020-01-03 11:30:35 +0530

    work reduce

--- nano/lib/config.hpp
+++ nano/lib/config.hpp
@@ -73,8 +73,8 @@ public:
 	}
 
 	/** Network work thresholds. ~5 seconds of work for the live network */
-	static uint64_t const publish_full_threshold{ 0xffffffc000000000 };
-	static uint64_t const publish_beta_threshold{ 0xfffffc0000000000 }; // 16x lower than full
+	static uint64_t const publish_full_threshold{ 0xff00000000000000 };
+	static uint64_t const publish_beta_threshold{ 0xff00000000000000 }; // 16x lower than full
 	static uint64_t const publish_test_threshold{ 0xff00000000000000 }; // very low for tests
 
 	/** The network this param object represents. This may differ from the global active network; this is needed for certain --debug... commands */
